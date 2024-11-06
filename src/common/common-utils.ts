96b51b8bfd23021969fef42de11f9d51725059a4
SatyamSingh343 <satyamkr304@gmail.com>
2024-11-06 11:38:15 +0530

    second commit

--- src/common/common-utils.ts
+++ src/common/common-utils.ts
@@ -3,7 +3,7 @@ import type { Nil } from './common-types';
 export const APP_URL = 'https://next-shopper.vercel.app';
 export const APP_TITLE = 'next-shopper';
 export const APP_DESCRIPTION = `${APP_TITLE} is a simple fullstack e-commerce website demo built with Next.js`;
-export const APP_REPOSITORY_URL = 'https://github.com/Abhi96k';
+export const APP_REPOSITORY_URL = 'https://github.com/SatyamSingh343/21BAI10302_Nextjsecommerce';
 
 export const createMockArray = (length: number) => {
   // eslint-disable-next-line unicorn/prefer-spread
